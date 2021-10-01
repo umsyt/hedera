0e87e2737a15c4c34f2b0647ef568f380f40f1db
umsyt <75184495+umsyt@users.noreply.github.com>
2021-10-01 16:37:17 -0400

    Update 3game.js

--- decade/3game.js
+++ decade/3game.js
@@ -1,13 +1,19 @@
+// Yeah, you can try to find the answer from this, but try to do the level as intended
+// I promise, you won't regret it :)
+
 document.addEventListener("keydown", down);
 document.addEventListener("keyup", up);
 
-var cool = [84, 73, 77, 69, 87, 65, 82, 80]
+var cool = [77, 72, 83, 86, 80, 84, 82, 78, 79, 73, 65, 71, 89, 67, 70, 81, 74, 85, 69, 66, 68, 76, 75, 88, 90, 87]
+var sook = [27, 12, 33, 37, 28, 32, 19, 31]
 
 function down(k) {
-  if (cool.includes(k.keyCode)) {
+  var jooch = cool.indexOf(k.keyCode);
+  
+  if (sook.includes(37-jooch)) {
     document.getElementById("three").src = "3yay.jpg";
   }
-  else if (64 < k.keyCode && k.keyCode < 91) {
+  else if (cool.includes(k.keyCode)) {
     document.getElementById("three").src = "3awman.jpg";
   }
 }
